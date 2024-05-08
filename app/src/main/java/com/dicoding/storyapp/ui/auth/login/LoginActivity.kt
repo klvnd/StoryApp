0435215f4691d134212c534b790ee11bc9ec1960
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-05-09 01:30:37 +0700

    update: add animation

--- app/src/main/java/com/dicoding/storyapp/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/dicoding/storyapp/ui/auth/login/LoginActivity.kt
@@ -1,21 +1,74 @@
 package com.dicoding.storyapp.ui.auth.login
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.dicoding.storyapp.R
+import com.dicoding.storyapp.databinding.ActivityLoginBinding
+import com.dicoding.storyapp.ui.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.passwordEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                s?.let {
+                    if (it.length < 8) {
+                        binding.passwordEditText.error = "Password tidak boleh kurang dari 8 karakter";null
+                    } else {
+                        binding.passwordEditText.error = null
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        login()
+        playAnimation()
+    }
+
+    private fun login() {
+        binding.loginButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val login = ObjectAnimator.ofFloat(binding.loginButton, View.ALPHA, 1f).setDuration(500)
+        val title = ObjectAnimator.ofFloat(binding.titleTextView, View.ALPHA, 1f).setDuration(500)
+        val email = ObjectAnimator.ofFloat(binding.emailTextView, View.ALPHA, 1f).setDuration(500)
+        val emailInput = ObjectAnimator.ofFloat(binding.emailEditTextLayout, View.ALPHA, 1f).setDuration(500)
+        val password = ObjectAnimator.ofFloat(binding.passwordTextView, View.ALPHA, 1f).setDuration(500)
+        val passwordInput = ObjectAnimator.ofFloat(binding.passwordEditTextLayout, View.ALPHA, 1f).setDuration(500)
+
+        val togetherEmail = AnimatorSet().apply {
+            playTogether(email, emailInput)
+        }
+
+        val togetherPassword = AnimatorSet().apply {
+            playTogether(password, passwordInput)
+        }
+
+        AnimatorSet().apply {
+            playSequentially(title, togetherEmail, togetherPassword, login)
+            start()
         }
     }
 }
