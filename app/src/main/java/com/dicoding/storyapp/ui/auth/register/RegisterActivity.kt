3bedcc629b0a6852deabba3486c244393521c676
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-05-09 01:31:18 +0700

    refactor:

---
+++ app/src/main/java/com/dicoding/storyapp/ui/auth/register/RegisterActivity.kt
@@ -0,0 +1,51 @@
+package com.dicoding.storyapp.ui.auth.register
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.storyapp.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.passwordEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                s?.let {
+                    if (it.length < 8) {
+                        binding.passwordEditText.error = "Password tidak boleh kurang dari 8 karakter";null
+                    } else {
+                        binding.passwordEditText.error = null
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.registerButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+
+            AlertDialog.Builder(this).apply {
+                setTitle("Yeah!")
+                setMessage("Akun dengan $email sudah jadi nih. Yuk, login dan belajar coding.")
+                setPositiveButton("Lanjut") { _, _ ->
+                    finish()
+                }
+                create()
+                show()
+            }
+        }
+    }
+}
