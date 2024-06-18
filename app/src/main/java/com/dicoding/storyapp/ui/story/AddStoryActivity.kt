9e4ac6bd6ff54e5b2ea9023dc65b4cbd879e1951
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-06-18 19:01:14 +0700

    add function btn gallery

--- app/src/main/java/com/dicoding/storyapp/ui/story/AddStoryActivity.kt
+++ app/src/main/java/com/dicoding/storyapp/ui/story/AddStoryActivity.kt
@@ -1,17 +1,28 @@
 package com.dicoding.storyapp.ui.story
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.dicoding.storyapp.R
+import com.dicoding.storyapp.databinding.ActivityAddStoryBinding
 
 class AddStoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddStoryBinding
+    private var currentImageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_story)
+
+        // Initialize the binding
+        binding = ActivityAddStoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar?.setCustomView(R.layout.custom_action_bar)
@@ -19,10 +30,30 @@ class AddStoryActivity : AppCompatActivity() {
         val titleTextView = supportActionBar?.customView?.findViewById<TextView>(R.id.action_bar_title)
         titleTextView?.text = "Add Story"
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding.btnGallery.setOnClickListener {
+            startGallery()
         }
+    }
+
+    private fun startGallery() {
+        launcherGallery.launch(
+            PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+        )
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            binding.ivAddStoryImage.setImageURI(uri)
+            showToast("Image selected: $uri")
+        } else {
+            showToast("No image selected")
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
