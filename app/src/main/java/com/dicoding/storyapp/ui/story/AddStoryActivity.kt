6017cb10c399d5f7a13447be4f242d23144fefe5
Kelvin Dwipangga <144301420+klvnd@users.noreply.github.com>
2024-06-21 20:04:52 +0700

    update: add camera Intent

--- app/src/main/java/com/dicoding/storyapp/ui/story/AddStoryActivity.kt
+++ app/src/main/java/com/dicoding/storyapp/ui/story/AddStoryActivity.kt
@@ -1,20 +1,29 @@
 package com.dicoding.storyapp.ui.story
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.dicoding.storyapp.R
 import com.dicoding.storyapp.databinding.ActivityAddStoryBinding
+import java.io.ByteArrayOutputStream
 
 class AddStoryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddStoryBinding
 
     private var currentImageUri: Uri? = null
+    val REQUEST_CODE_CAMERA = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +42,7 @@ class AddStoryActivity : AppCompatActivity() {
         }
 
         binding.btnCamera.setOnClickListener {
-            startCamera()
+            checkCameraPermission()
         }
 
         binding.btnUpload.setOnClickListener {
@@ -45,8 +54,63 @@ class AddStoryActivity : AppCompatActivity() {
         showToast("Upload clicked")
     }
 
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            startCamera()
+        } else {
+            requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+
+    private val requestCameraPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            startCamera()
+        } else {
+            showToast("Camera permission is required to use the camera.")
+        }
+    }
+
     private fun startCamera() {
-        showToast("Camera clicked")
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_CODE_CAMERA)
+        }catch (e: Exception) {
+            showToast("Error: ${e.message}")
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as? Bitmap
+            imageBitmap?.let {
+                val uri = getImageUri(this, it)
+                uri?.let {
+                    currentImageUri = it
+                    showImage(it)
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun getImageUri(inContext: Context, inImage: Bitmap): Uri? {
+        val bytes = ByteArrayOutputStream()
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = MediaStore.Images.Media.insertImage(
+            inContext.contentResolver,
+            inImage,
+            "Title",
+            null
+        )
+        return Uri.parse(path)
     }
 
     private fun startGallery() {
@@ -58,14 +122,14 @@ class AddStoryActivity : AppCompatActivity() {
     ) { uri: Uri? ->
         if (uri != null) {
             currentImageUri = uri
-            showImage()
+            showImage(uri)
         } else {
             showToast("No media selected")
         }
     }
 
-    private fun showImage() {
-        currentImageUri?.let {
+    private fun showImage(uri: Uri?) {
+        uri?.let {
             binding.ivAddStoryImage.setImageURI(it)
         }
     }
